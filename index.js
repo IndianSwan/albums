5f6e045be5fb5e7b283fbec4d3cbfba2127bbef5
Vishal Mahajan <mahajanvishalkumar@gmail.com>
2019-05-31 01:23:25 +0530

    some changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* eslint-disable brace-style */
 /* eslint-disable arrow-body-style */
 
-        import { AppRegistry } from 'react-native';
-        import App from './App';
-        import { name as appName } from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
 
 
 // //Render it in Device 
