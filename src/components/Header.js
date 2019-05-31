c69eb3bf81412d40feb16dd9b87ad3449b5ae148
Vishal Mahajan <mahajanvishalkumar@gmail.com>
2019-05-31 23:37:59 +0530

    Header Design

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,31 @@ import { Text, View } from 'react-native';
 // Make a component 
 
 const Header = (props) => {
-const { textStyle, viewStyle } = styles;
-return (
-    <View style={viewStyle}> 
-        <Text style={textStyle} >{props.headerText} </Text>
-    </View>);
-};  
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText} </Text>
+        </View>);
+};
 
 const styles = {
-        textStyle: {
-            fontSize: 25
-        },
-        viewStyle: { 
-            backgroundColor: '#F8F8F8'
-        }
+    textStyle: {
+        fontSize: 20,
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+
+    }
 };
 
 // Make a component available to other parts 
