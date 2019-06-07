139d62a5186cba84d95f715200e9b7b9359cf1c8
Vishal Mahajan <mahajanvishalkumar@gmail.com>
2019-06-08 01:33:01 +0530

    Card done

--- src/components/Albumdetail.js
+++ src/components/Albumdetail.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
 const AlbumDetail = (props) => {
-return (
-    <View>
-        <Text>{props.album.title}</Text>
-    </View>
-);
+    return (
+        <Card>
+            <Text>{props.album.title}</Text>
+        </Card>
+    );
 };
 
 export default AlbumDetail;
+
